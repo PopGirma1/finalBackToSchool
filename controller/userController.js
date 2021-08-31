3fefe2ff0899ad6058e507616613402861824d3f
belayhun a <belayhun24arage@gmail.com>
2021-08-30 15:17:41 -1200

    modifiy register for event via voice

--- controller/userController.js
+++ controller/userController.js
@@ -10,12 +10,20 @@ const registerForEventViaInfo=async (req,res)=>{
         const user=await EventUser.create({name,email,phone,company,Additional_info,registerd_for_event})
         await event.participants.push(user._id)  
         await event.save()
-        console.log("The Event is::::::::::",event)
-        console.log("The user is:::::::::",user)
     }catch(error) {console.log("Errrorrrr::",error)}
 }
-const registerForEventViaVoice=(req,res)=>{
 
+const registerForEventViaVoice=(req,res)=>{
+    const event=await Event.findById(req.params.event)
+    const registerd_for_event=event._id
+    const {voice}=req.body
+    try {
+        const user=await EventUser.create({voice,registerd_for_event})
+        await event.participants.push(user._id)
+        await event.save()
+    } catch (error) {
+        console.log(error)
+    }
 }
 const registerForHackaton=async (req,res)=>{
     const {FirstName,LastName,TelMob,Email,Gender,facebookLink,telegramLink,linkedInLink,twitterLink,instagrameLink,course,level,year,school,topic,currentStatus,whyInetested,futureVision,hasLabtop}=req.body
